8efa6aca071a660691c6671eb320adcf111d6733
ezginaztekin <80275365+ezginaztekin@users.noreply.github.com>
2021-05-26 18:01:39 +0300

    Create Historical Ciphers.py

--- Historical Ciphers.py
+++ Historical Ciphers.py
@@ -1,5 +1,5 @@
-# Python codes for Caesar Cipher for encryption 
-def ceaserencrypt(text, s):
+# Python codes for Caesar Cipher encryption
+def ceaser_cipher_encrypt(text, shift):
 
     t = list(text)
     textt=[]
@@ -7,12 +7,12 @@ def ceaserencrypt(text, s):
 
      #Encrypting uppercase characters
        if (t[i].isupper()):
-          k = chr((ord(t[i]) + int(s) - 65) % 26 + 65) 
+          k = chr((ord(t[i]) + int(shift) - 65) % 26 + 65) 
           textt.append(k)
 
       # Encrypting lowercase characters
        elif (t[i].islower()):
-          k =  chr((ord(t[i]) + int(s) - 97) % 26 + 97)
+          k =  chr((ord(t[i]) + int(shift) - 97) % 26 + 97)
           textt.append(k)
 
       # Checking empty list index 
@@ -22,11 +22,68 @@ def ceaserencrypt(text, s):
     texttt =""
     return (texttt.join(textt))
  
-#checking the ceaserencrypt function
-text = input("Please give the plaintext : ")      #CEASER CIPHER
-s = input("Please give the shift number : ")      #13
+#checking the ceaser_cipher_encrypt function  
+ceaser_text = input("Please give the plaintext : ")      # input: naz
+ceaser_shift = input("Please give the shift number : ")  # input: 11
 
 
-print ("Ciphertext : ", ceaserencrypt(text, s))
+print ("Ciphertext : ", ceaser_cipher_encrypt(ceaser_text, ceaser_shift)) #output: ylk
+
+
+# Python codes for Hill Cipher encryption
+
+import numpy as np
+def hill_cipher_encrypt(text, key):
+
+  a = len(text)
+
+# Generating vector for the key  
+  key_matrix = np.zeros((a,a),dtype=int)
+  
+# Generating vector for the plaintext
+  text_matrix = np.zeros(a,dtype=int).T
+  
+# Generating vector for the cipher
+  cipher_matrix = np.zeros(a,dtype=int).T
+  
+# Getting key matrix from the key string 
+  k = 0
+  for i in range(3):
+      for j in range(3):
+          key_matrix[i, j] = ord(key[k]) % 65
+          k += 1 
+ 
+# Getting vector from the text string
+  for i in range(3):
+      text_matrix[i] = ord(text[i]) % 65
+  
+# Getting the encrypted vector with key metrix and text matrix
+  for i in range(3):
+    for j in range(3):
+      cipher_matrix[i] += (key_matrix[i, j] * text_matrix[j])
+    cipher_matrix[i] = cipher_matrix[i] % 26
+
+# Generating the encrypted text from the encrypted vector
+  cipher_text = []
+  for i in range(3):
+      cipher_text.append(chr(cipher_matrix[i] + 65))
+  
+    # Returning the ciphertext
+  return ("".join(cipher_text))
+  
+#checking the hill_cipher_encrypt function 
+hill_text = input("Please give the plaintext : ")      
+hill_key = input("Please give the shift number : ")    
+  
+print("the ciphertext:", hill_cipher_encrypt(hill_text, hill_key) ) 
+
+
+
+
+
+
+
+
+
 
 
